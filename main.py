c4611e5bd643c2620192d48eebdaffe38345161d
Sam Paxton <samw.paxton@gmail.com>
2023-03-02 17:23:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+
+
+net = Turtle()
+
+
+scoreboard = Scoreboard()
+
+ball = Ball()
+
+
+
+
+
+
+
+screen.exitonclick()
