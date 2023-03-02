c69f3688de64779044ede623741d18372abc1251
Sam Paxton <samw.paxton@gmail.com>
2023-03-02 18:35:56 +0000

    Screen, net and paddle features added

--- main.py
+++ main.py
@@ -4,15 +4,47 @@ from ball import Ball
 from scoreboard import Scoreboard
 
 screen = Screen()
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
 
 
 net = Turtle()
+net.ht()
+net.pensize(5)
+net.penup()
+net.pencolor("white")
+net.goto(0,300)
+net.seth(270)
+dashes = 20
+for _ in range(dashes):
+    down = net.isdown()
+    if down:
+        net.pu()
+    else:
+        net.pd()
+    net.fd(30)
 
+l_paddle = Paddle(position="left")
+r_paddle = Paddle(position="right")
+
+screen.listen()
+screen.onkeypress(key="w", fun=l_paddle.up)
+screen.onkeypress(key="s", fun=l_paddle.down)
+screen.onkeypress(key="Up", fun=r_paddle.up)
+screen.onkeypress(key="Down", fun=r_paddle.down)
 
 scoreboard = Scoreboard()
 
 ball = Ball()
+
+
+
+game_on=True
+while game_on:
+    screen.update()
+
 
 
 
--- paddle.py
+++ paddle.py
@@ -1,6 +1,31 @@
 from turtle import Turtle
 
+LEFT = (-350,0)
+RIGHT = (350,0)
+
 class Paddle(Turtle):
     
-    def __init__(self):
-        a = 0
+    def __init__(self, position):
+        super().__init__()
+        self.position = position
+        self.shape("square")
+        self.color("white")
+        self.speed("fast")
+        self.pu()
+        if position == "left":
+            self.goto(LEFT)
+        if position == "right":
+            self.goto(RIGHT)
+        self.shapesize(stretch_wid=5, stretch_len=0.3)
+    
+    def up(self):
+        print(f"{self.position} paddle UP")
+        self.goto(self.xcor(), self.ycor()+30)
+        
+    def down(self):
+        print(f"{self.position} paddle DOWN")
+        self.goto(self.xcor(), self.ycor()-30)
+        
+        
+        
+    
